a7ac3d1eb22351f5eda97009ef89014099054724
Гудков Сергей Андреевич <60447026+DekartVan@users.noreply.github.com>
2024-01-05 10:38:48 +0400

    Add files via upload

--- kernel.cu
+++ kernel.cu
@@ -5,6 +5,8 @@
 #include "EasyBMP.h"
 #include <time.h>
 #include "ray.h"
+#include "hitable_list.h"
+#include "sphere.h"
 
 // limited version of checkCudaErrors from helper_cuda.h in CUDA examples
 #define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
@@ -19,27 +21,51 @@ void check_cuda(cudaError_t result, char const* const func, const char* const fi
     }
 }
 
-__device__ vec3 color(const ray& r) {
-    vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5f * (unit_direction.y() + 1.0f);
-    return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+__device__ vec3 color(const ray& r, hitable** world) {
+    hit_record rec;
+    if ((*world)->hit(r, 0.0, FLT_MAX, rec)) {
+        return 0.5f * vec3(rec.normal.x() + 1.0f, rec.normal.y() + 1.0f, rec.normal.z() + 1.0f);
+    }
+    else {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5f * (unit_direction.y() + 1.0f);
+        return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    }
 }
 
-__global__ void render(vec3* fb, int max_x, int max_y, vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin) {
+
+__global__ void render(vec3 *fb, int max_x, int max_y,
+                       vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin,
+                       hitable **world) {
+
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
-    if ((i >= max_x) || (j >= max_y)) return;
-
-    int pixel_index = j * max_x + i;
+    if((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j*max_x + i;
     float u = float(i) / float(max_x);
     float v = float(j) / float(max_y);
-    ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-    fb[pixel_index] = color(r);
+    ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+    fb[pixel_index] = color(r, world);
 }
 
+__global__ void create_world(hitable** d_list, hitable** d_world) {
+    if (threadIdx.x == 0 && blockIdx.x == 0) {
+        *(d_list) = new sphere(vec3(0, 0, -1), 0.5);
+        *(d_list + 1) = new sphere(vec3(0, -100.5, -1), 100);
+        *d_world = new hitable_list(d_list, 2);
+    }
+}
+
+__global__ void free_world(hitable** d_list, hitable** d_world) {
+    delete* (d_list);
+    delete* (d_list + 1);
+    delete* d_world;
+}
+
+
 int main() {
-    int nx = 1920;
-    int ny = 1080;
+    int nx = 1200;
+    int ny = 600;
     int tx = 8;
     int ty = 8;
 
@@ -53,6 +79,15 @@ int main() {
     vec3* fb;
     checkCudaErrors(cudaMallocManaged((void**)&fb, fb_size));
 
+    // make our world of hitables
+    hitable** d_list;
+    checkCudaErrors(cudaMalloc((void**)&d_list, 2 * sizeof(hitable*)));
+    hitable** d_world;
+    checkCudaErrors(cudaMalloc((void**)&d_world, sizeof(hitable*)));
+    create_world << <1, 1 >> > (d_list, d_world);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+
     clock_t start, stop;
     start = clock();
 
@@ -63,7 +98,8 @@ int main() {
         vec3(-2.0, -1.0, -1.0),
         vec3(4.0, 0.0, 0.0),
         vec3(0.0, 2.0, 0.0),
-        vec3(0.0, 0.0, 0.0));    
+        vec3(0.0, 0.0, 0.0),
+        d_world);
     checkCudaErrors(cudaGetLastError());
     checkCudaErrors(cudaDeviceSynchronize());
     stop = clock();
@@ -84,12 +120,18 @@ int main() {
             pixel.Green = int(255.99 * fb[pixel_index].g());
             pixel.Blue = int(255.99 * fb[pixel_index].b());
             pixel.Alpha = 0;
-            image.SetPixel(i, j, pixel);
+            image.SetPixel(j, i, pixel);
 
 
         }
     }
     image.WriteToFile("output.bmp");
 
+    checkCudaErrors(cudaDeviceSynchronize());
+    free_world << <1, 1 >> > (d_list, d_world);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaFree(d_list));
+    checkCudaErrors(cudaFree(d_world));
     checkCudaErrors(cudaFree(fb));
+    cudaDeviceReset();
 }
